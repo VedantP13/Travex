dc0de94f2f6acb5788d91b46f38b9a965cdade14
Vedant Patel <vedantdpatel13@gmail.com>
2026-06-11 05:52:07 +0000

    Just to be clear I want to make this as a mobile app, alsong with the we

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,4 +1,6 @@
 
+'use client';
+
 import { initializeApp, getApps, FirebaseApp } from "firebase/app";
 import { getFirestore, Firestore } from "firebase/firestore";
 import { getAuth, Auth } from "firebase/auth";
